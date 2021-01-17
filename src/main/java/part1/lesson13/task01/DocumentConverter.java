548e2ecacb430cf218d981de57a1f42c91dfb4ad
deNNi <dns.nikonorov@gmail.com>
2021-01-17 23:10:38 +0400

    Fixes after feedback Refactor to maven project Added slf4j logger

--- src/main/java/part1/lesson13/task01/DocumentConverter.java
+++ src/main/java/part1/lesson13/task01/DocumentConverter.java
@@ -1,5 +1,8 @@
 package part1.lesson13.task01;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.util.Scanner;
 
@@ -11,26 +14,37 @@ public class DocumentConverter {
     float amount;
     float all;
 
+    private static Logger logger = LoggerFactory.getLogger(DocumentConverter.class);
+
     public DocumentConverter() {
     }
 
     public void readFromFile() {
+        logger.info("Указываем с консоли путь к файлу");
         System.out.print("Введите путь к файлу: ");
+
         try(BufferedReader reader = new BufferedReader(new FileReader(new Scanner(System.in).nextLine()))) {
+            logger.info("Считываем построчно данные из файла");
             System.out.println("=====================================================");
             while (true){
+                logger.debug("Считываем данные для переменной \"name\"");
                 name = reader.readLine();
+                logger.debug("Данные для переменной \"name\" - {}", name);
                 quantity = Float.parseFloat(reader.readLine());
+                logger.debug("Данные для переменной \"quantity\" - {}", quantity);
                 price = Float.parseFloat(reader.readLine());
+                logger.debug("Данные для переменной \"price\" - {}", price);
                 amount= quantity*price;
                 all = all + amount;
-                System.out.format("%-20s %-5.2f X %-7.2f =%-1.2f \n", name, quantity, price, amount);
+                System.out.format("%-20s %-7.2f X %-5.2f =%-1.2f \n", name, price, quantity, amount);
             }
         }catch (IOException e) {
+            logger.error("Ошибка Ввода");
             e.printStackTrace();
         } catch (NullPointerException exception){
+            logger.warn("Закончились данные");
             System.out.println("=====================================================");
-            System.out.printf("Итого: %f", all);
+            System.out.printf("%-30s Итого: %.2f\n", "", all);
         }
     }
 }
